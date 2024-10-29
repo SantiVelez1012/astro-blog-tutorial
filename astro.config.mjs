740eda911c674487db2dd9e1e8d7cb6457469601
Santiago Velez <santivekez1012@gmail.com>
2024-10-29 14:07:53 -0500

    refactoring posts and improving code examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://harmonious-parfait-30cc51.netlify.app/"
+});
